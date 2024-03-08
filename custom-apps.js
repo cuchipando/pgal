3790d7b6fdb3d7e3a4c629566e915a4d48b6cd1c
cuchipando <158587372+cuchipando@users.noreply.github.com>
2024-03-08 13:00:38 +0000

    Update custom-apps.js
    Add wow gala

--- custom-apps.js
+++ custom-apps.js
@@ -1,6 +1,7 @@
 //your list of apps goes below
 let apps = [    
   {slug: "lucas-de-poap-studio-1", id: "gsv3QyAnHMNtgKQNBIeP"},
+  {slug: "wow-gala-1", id: "XTngDxIgtm0f1PrSIbb4"},
   {slug: "rug-radio-1", id: "AiO3rk2BPaD3xxlbaEIk"}
 ];   
 //your list of apps goes above
