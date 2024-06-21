422cce4d610e1fc814ef2232fb5af9f8c4b2cbdb
cuchipando <158587372+cuchipando@users.noreply.github.com>
2024-06-21 09:16:19 +0100

    Update custom-apps.js

--- custom-apps.js
+++ custom-apps.js
@@ -6,9 +6,10 @@ let apps = [
 ];   
 //your list of apps goes above
 let currentSlug = window.location.pathname.split("/")[4] || ""; 
-let currentApp = apps.find(o => o.slug === currentSlug) || apps[0];   
-let appId = currentApp.id;   
-let script = document.createElement("script");   
-let scriptUrl = "https://progressier.app/"+appId+"/script.js";   
-script.setAttribute("src", scriptUrl);   
-document.querySelector('body').appendChild(script);
+let currentApp = apps.find(o => o.slug === currentSlug); // removed the defaulting to apps[0]
+if (currentApp) {
+  let appId = currentApp.id;   
+  let script = document.createElement("script");   
+  let scriptUrl = "https://progressier.app/"+appId+"/script.js";   
+  script.setAttribute("src", scriptUrl);   
+  document.querySelector('body').appendChild(script);
