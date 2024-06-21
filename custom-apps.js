40105c6f6606946cd6df9aadf8b3d0921411ebbb
cuchipando <158587372+cuchipando@users.noreply.github.com>
2024-06-21 09:26:01 +0100

    Update custom-apps.js

--- custom-apps.js
+++ custom-apps.js
@@ -6,22 +6,26 @@ let apps = [
 ];   
 // Your list of apps goes above
 
-// Capture the current slug from the URL
-let currentSlug = window.location.pathname.split("/")[4] || ""; 
-console.log("Current Slug: ", currentSlug);
+try {
+  // Capture the current slug from the URL
+  let currentSlug = window.location.pathname.split("/")[4] || ""; 
+  console.log("Current Slug: ", currentSlug);
 
-// Find the app that matches the current slug
-let currentApp = apps.find(o => o.slug === currentSlug);
-console.log("Current App: ", currentApp);
+  // Find the app that matches the current slug
+  let currentApp = apps.find(o => o.slug === currentSlug);
+  console.log("Current App: ", currentApp);
 
-// If a matching app is found, create and append the script
-if (currentApp) {
-  let appId = currentApp.id;   
-  let script = document.createElement("script");   
-  let scriptUrl = "https://progressier.app/"+appId+"/script.js";   
-  script.setAttribute("src", scriptUrl);   
-  document.querySelector('body').appendChild(script);
-  console.log("Script appended with URL: ", scriptUrl);
-} else {
-  console.log("No matching app found for the current slug.");
+  // If a matching app is found, create and append the script
+  if (currentApp) {
+    let appId = currentApp.id;   
+    let script = document.createElement("script");   
+    let scriptUrl = "https://progressier.app/" + appId + "/script.js";   
+    script.setAttribute("src", scriptUrl);   
+    document.querySelector('body').appendChild(script);
+    console.log("Script appended with URL: ", scriptUrl);
+  } else {
+    console.log("No matching app found for the current slug.");
+  }
+} catch (error) {
+  console.error("An error occurred: ", error);
 }
