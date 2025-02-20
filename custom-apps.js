249e47f2d24b85790c23a40bd1e31c56af260cfd
cuchipando <158587372+cuchipando@users.noreply.github.com>
2025-02-20 18:23:43 +0000

    Update custom-apps.js

--- custom-apps.js
+++ custom-apps.js
@@ -2,7 +2,8 @@
 let apps = [    
   {slug: "lucas-de-poap-studio-1", id: "OZbbJakbL7SqXQwrmfUa"},
   {slug: "mercedes-benz-consulting-20-1", id: "u2AkGwcEUNOVg3CjB6ZP"},
-  {slug: "merge-madrid-1", id: "w7wKQFv6wdZbMX432y14"}
+  {slug: "merge-madrid-1", id: "w7wKQFv6wdZbMX432y14"},
+  {slug: "linea-1", id: "KpqJ5VTW2vZgSAozZsSK"}
   
 ];   
 // Your list of apps goes above
